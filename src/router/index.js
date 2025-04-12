64a124208a48ff5aad9db303fb2eb343c470fe09
Van Villanueva <vanv69200@gmail.com>
2025-04-12 23:28:19 +0800

    DefaultDB(carlotirador

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Dashboard from '@/views/Dashboard.vue' // ← import your Dashboard
 import Login from '@/views/LogIn.vue'
 import Signup from '@/views/SignUp.vue'
 
+
 const routes = [
-  { path: '/', redirect: '/login' },     // 👈 Default route should redirect to /login
+  { path: '/', redirect: '/Dashboard' }, // ← now redirect root to dashboard
+  { path: '/Dashboard', component: Dashboard }, // ← add this route
   { path: '/login', component: Login },
   { path: '/signup', component: Signup }
+ 
 ]
 
 const router = createRouter({
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+
+    <template>
+  <div>
+    <h1>This is the Dashboard</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+
